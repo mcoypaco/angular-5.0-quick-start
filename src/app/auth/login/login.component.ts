0a87d31ed0b59ad50c59e72cb11fceb7064a04ca
Marco Paco <marco.paco@personiv.com>
2017-10-21 05:19:09 +0800

    Loading indicator on login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import 'rxjs/add/operator/finally';
 
 import { AuthService } from '../auth.service';
 import { AccessTokenService } from '../access-token.service';
+import { ExceptionService } from '../../core/exception.service';
 import { LoginFormQuestionsService } from './login-form-questions.service';
 import { QuestionBase } from '../../shared/question-base';
 import { QuestionControlService } from '../../shared/question-control.service';
@@ -15,12 +17,15 @@ import { QuestionControlService } from '../../shared/question-control.service';
   providers: [LoginFormQuestionsService],
 })
 export class LoginComponent implements OnInit {
+  busy: boolean;
+  invalidCredentials: boolean;
   form: FormGroup;
   questions: QuestionBase<any>[];
   
   constructor(
     private auth: AuthService,
     private accessToken: AccessTokenService,
+    private exception: ExceptionService,
     private questionControl: QuestionControlService, 
     private questionSource: LoginFormQuestionsService,
     private router: Router,
@@ -35,13 +40,23 @@ export class LoginComponent implements OnInit {
    * Attempt login with user credentials
    */
   login() {
-    if(this.form.valid) {
+    if(this.form.valid && !this.busy) {
+      this.busy = true; 
+      this.invalidCredentials = false;
+
       this.auth.login(this.form.get('email').value, this.form.get('password').value)
+        .finally(() => this.busy = false)
         .subscribe(
           apiAccess => this.accessToken.store('apiAccess', apiAccess),
-          error => console.log(error),
+          error => this.handleError(error),
           () => this.router.navigate(['/'])
         );
     }
+  }
+
+  handleError(error) {
+    if(error.status != 401) return this.exception.handle(error);
+
+    this.invalidCredentials = true;
   }
 }
